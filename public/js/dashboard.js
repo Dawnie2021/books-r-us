6e4b903176fb1639ecbd849ccf71dc0368341d35
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2024-02-06 13:21:57 -0500

    Latest

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,18 @@
-card = document.querySelector(".card-base");
-parent = document.querySelector("#parent");
+const Handlebars = require("handlebars");
+
+/* let card = document.querySelector(".card-base");
+let parent = document.querySelector("#parent");
 
 card.remove();
 
 for (let i = 0; i < 5; i++) {
     parent.appendChild(card.cloneNode(true));
-}
+} */
+
+let elem = document.querySelector("#template");
+let template = Handlebars.compiles(elem);
+
+let data = template({ data: "work please" });
+
+let toAdd = document.querySelector(".hero");
+toAdd.innerHTML += data;
