b214b1af50185a9e26b33ecad04dd8975959cb65
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2024-02-08 18:57:45 -0500

    Book routes added

--- routes/api/bookRoutes.js
+++ routes/api/bookRoutes.js
@@ -1,20 +1,46 @@
-// const router = require('express').Router();
-// const Book = require('../../models/Book');
+const router = require('express').Router();
+const Book = require('../../models/Book');
 
-// router.post('/', async (req, res) => {
-//   try { 
-//     const bookData = await Book.create({
-//    book_title: req.body.book_title,
-//     book_description: req.body.book_description,
-//    author_name: req.body.author_name,
-//    genre: req.body-genre
-//   });
+router.post('/', async (req, res) => {
+    try {
+        const bookData = await Book.create({
+            book_title: req.body.book_title,
+            book_description: req.body.book_description,
+            author_name: req.body.author_name,
+            genre: req.body - genre
+        });
 
-//   res.redirect('/');
-// } catch (err) {
-//   res.status(400).json(err);
-// }
-// });
+        res.redirect('/');
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
+router.get('/', async (req, res) => {
+    console.log("START");
+    try {
+        const dbBookData = await Book.findAll();
 
-// module.exports = router;
+        res.status(200).json(dbBookData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const dbBookData = await Book.findByPk(req.params.id, {
+
+        });
+
+        const book = dbBookData.get({ plain: true });
+        res.status(200).json(book);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+
