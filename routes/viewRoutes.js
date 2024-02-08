13beda68d026acf130220ea728845b5156e7da90
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2024-02-08 18:59:10 -0500

    Dashboard Changes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,49 +3,41 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, async (req, res) => {
-  try {
+router.get('/', /* withAuth, async */ (req, res) => {
+
+  res.render('index', {
+/*     users,
+    loggedIn: req.session.loggedIn, */
+  });
+
+/*   try {
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
       order: [['name', 'ASC']],
     });
 
     const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('index', {
-      users,
-      loggedIn: req.session.loggedIn,
-    });
   } catch (err) {
     res.status(500).json(err);
-  }
+  } */
 });
 
 router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
+/*   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
-
+ */
   res.render('login');
 });
 
 router.get('/signup', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-
   res.render('signup');
 });
 
-router.get('/dashboard', withAuth, (req, res) => {
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-    return;
-  }
-
-  res.render('dashboard');
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard', // template name
+  { book: { title: 'Beans and Rice are Good' }},);
 });
 
 
