31c2dd3f3fdad6febc5d4ce2ef25eea55150cd1c
JacksonByrd7 <jackson.byrd7@gmail.com>
2024-02-08 20:15:26 -0500

    got books to display on dashboard

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,16 +1,16 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
-
-router.get('/', /* withAuth, async */ (req, res) => {
-
-  res.render('index', {
-/*     users,
+router.get(
+  "/",
+  /* withAuth, async */ (req, res) => {
+    res.render("index", {
+      /*     users,
     loggedIn: req.session.loggedIn, */
-  });
+    });
 
-/*   try {
+    /*   try {
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
       order: [['name', 'ASC']],
@@ -20,25 +20,34 @@ router.get('/', /* withAuth, async */ (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   } */
-});
+  }
+);
 
-router.get('/login', (req, res) => {
-/*   if (req.session.loggedIn) {
+router.get("/login", (req, res) => {
+  /*   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
  */
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
-  res.render('signup');
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard', // template name
-  { book: { title: 'Beans and Rice are Good' }},);
+router.get("/dashboard", (req, res) => {
+  res.render(
+    "dashboard", // template name
+    {
+      id: 1,
+      book_name: "The Da Vinci Code",
+      book_author: "Dan Brown",
+      book_description:
+        "Follows symbologist Robert Langdon as he unravels a murder mystery in the Louvre Museum, delving into a conspiracy involving the Catholic Church and the search for the Holy Grail.",
+      genre: "mystery",
+    }
+  );
 });
-
 
 module.exports = router;
