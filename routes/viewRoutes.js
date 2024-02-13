98a410cc747709e5baf24397ad6a0097758ce146
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2024-02-12 21:32:46 -0500

    Navbar changes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,37 +2,22 @@ const router = require("express").Router();
 const { User, Book } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get(
-  "/",
-  /* withAuth, async */ (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/dashboard');
-      return;
-    }
-    res.render("index", {
-      /*     users,
-    loggedIn: req.session.loggedIn, */
-    });
-
-    /*   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-  } catch (err) {
-    res.status(500).json(err);
-  } */
-  }
-);
-
-router.get("/login", (req, res) => {
-    if (req.session.loggedIn) {
+router.get("/", (req, res) => {
+  if (req.session.loggedIn) {
     res.redirect('/dashboard');
     return;
   }
- 
+  res.render("index", {
+  });
+}
+);
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/dashboard');
+    return;
+  }
+
   res.render("login");
 });
 
@@ -44,20 +29,44 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get("/dashboard", async (req, res) => {
+router.get("/dashboard/all", async (req, res) => {
   if (!req.session.loggedIn) {
     res.redirect('/');
     return;
   }
+
   const bookData = await Book.findAll();
   const books = bookData.map(o => o.get());
 
   res.render(
     "dashboard", // template name
-    
+
     {
       books
     }
+  );
+});
+
+router.get("/dashboard", async (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  const bookData = await Book.findAll();
+  const all = bookData.map(o => o.get());
+
+  
+
+  /* if (books.count <= 0)
+    res.redirect("/all"); */
+
+  res.render(
+    "dashboard", // template name
+
+/*     {
+      books
+    } */
   );
 });
 
@@ -68,8 +77,9 @@ router.get("/books/:id", async (req, res) => {
   res.render(
     "book", // template name
     {
-      book, 
+      book,
     }
   );
 });
+
 module.exports = router;
